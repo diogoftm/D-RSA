98fabf815ce64e80c1abb36551b89346403ace2f
Tiago Silvestre <tiago.silvestre@ua.pt>
2023-11-03 11:59:39 +0000

    update generator files

--- cpp/generator.cpp
+++ cpp/generator.cpp
@@ -1,13 +1,35 @@
 #include "generator.h"
+#include <openssl/evp.h>
 
-Generator::Generator(std::string password, std::string confusionString, uint16_t iterationCount) {
-
+Generator::Generator(GeneratorArgs args) {
+    //PKCS5_PBKDF2_HMAC()
+    this->args = args;
 }
 
 void Generator::setup() {
 
+    Seed bootstrapSeed, iterationSeed;
+    findBootstrapSeed(this->args, bootstrapSeed);
+    initializeGenerator(bootstrapSeed);
+
+    for(uint32_t i=0;i<this->args.IC;i++) {
+        findNextSeed(iterationSeed);
+        initializeGenerator(iterationSeed);
+    }
 }
 
 void Generator::nextBlock(uint8_t* block, uint32_t blockLength) {
+
+}
+
+void Generator::findBootstrapSeed(const GeneratorArgs& args, Seed& seed) {
+
+}
+
+void Generator::initializeGenerator(Seed& seed) {
+
+}
+
+void Generator::findNextSeed(Seed& seed) {
     
 }
--- cpp/generator.h
+++ cpp/generator.h
@@ -2,20 +2,24 @@
 
 #include <string>
 
-/*
-Usage:
-Generator g = Generator(...);
-g.setup();
-g.nextBlock(block, 512);
-...
 
-*/
+
+struct Seed {
+    uint8_t bytes[32];
+};
+
+struct GeneratorArgs {
+    std::string PW;
+    std::string CS;
+    uint16_t IC;
+};
+
 
 class Generator {
 
     public:
 
-        Generator(std::string password, std::string confusionString, uint16_t iterationCount);
+        Generator(GeneratorArgs args);
 
         // Runs setup algorithm
         void setup();
@@ -24,5 +28,9 @@ class Generator {
 
     private:
 
-        
+        void findBootstrapSeed(const GeneratorArgs& args, Seed& seed);
+        void initializeGenerator(Seed& seed);
+        void findNextSeed(Seed& seed);
+
+        GeneratorArgs args;
 };
